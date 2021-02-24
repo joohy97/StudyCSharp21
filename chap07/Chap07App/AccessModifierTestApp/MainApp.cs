fa9976f2986650ab6be858a39ac3ca41bb657ed4
joohy <joohyun104@gmail.com>
2021-02-24 17:06:03 +0900

    0224 17:05

---
+++ chap07/Chap07App/AccessModifierTestApp/MainApp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifierTestApp
+{
+    class Boiler
+    {
+        private int temp = 5;
+        public void SetTemp(int temp)
+        {
+            if (temp < 30 || temp > 60)
+            {
+                Console.WriteLine("물의 온도가 범위를 벗어났으므로 45℃로 온도를 설정합니다.");
+                this.temp = 59;
+                return;            
+            } 
+            else
+            {
+                this.temp = temp;
+            }
+            
+
+        }
+
+        public int GetTemp()
+        {
+            return this.temp;
+        }
+
+        public void TurnOnBoiler()
+        {
+            Console.WriteLine("보일러를 작동시킵니다.");
+        }
+
+        public void TurnOffBoiler()
+        {
+            Console.WriteLine("보일러를 중지시킵니다.");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Boiler kitturami = new Boiler();
+            
+            var currTemp = kitturami.GetTemp();
+            Console.WriteLine($"현재 온도는 {currTemp}℃ 입니다");
+
+            kitturami.SetTemp(40);
+            kitturami.TurnOnBoiler();
+            kitturami.SetTemp(59);
+
+            if (kitturami.GetTemp() >= 59)
+            {
+                kitturami.TurnOffBoiler();
+            }
+
+
+        }
+    }
+}
---
+++ chap07/Chap07App/ObjectTest2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Chap07App
+{
+    class ObjectTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("객체 테스트");
+
+            /*
+            Cat cat1 = new Cat(); //고양이 객체의 실체(instance) 생성
+            Cat cat1 = new Cat(Name: "야옹이", Color: "흰색")
+            //cat1.Name = "꼬미";
+            //cat1.Color = "태비";
+            cat1.Meow();
+
+            Cat kitty = new Cat();
+            kitty.Name = "헬로키티";
+            kitty.Color = "하얀색";
+            kitty.Meow();
+
+            Cat nero = new Cat();
+            nero.Name = "네로";
+            nero.Color = "검은색";
+            nero.Meow();
+            */
+
+            Cat yomi = new Cat("요미", "흰색", "암컷");
+            yomi.Meow();
+
+        }
+    }
+
+    class Cat
+    {
+        public Cat() { }
+
+        public Cat(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public Cat(string Name, string Color) : this(Name) //위의 생성자 Cat이 호출되어 Name이 초기화됨
+        {
+            //this.Name = Name;
+            this.Color = Color;
+        }
+
+        public Cat(string Name, string Color, string Gender) : this(Name, Color)
+        {
+            //this.Name = Name;
+            //this.Color = Color;
+            this.Gender = Gender;
+        }
+
+        public string Name;
+        public string Color;
+        public string Gender;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{this.Name}(색:{this.Color}/ 성별:{Gender}) : 야옹");
+        }
+    }
+}
