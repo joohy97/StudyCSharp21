700dcfc8af69098b0802703f2c8eb823ca605ebf
joohy <joohyun104@gmail.com>
2021-02-24 10:42:34 +0900

    0224 10:42

---
+++ chap06/Chapo06App/OverloadTestApp/Calculator.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace OverloadTestApp
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("계산기");
+
+            int x = Calculator.Plus(3, 4);
+            Console.WriteLine($"INT : 3 + 4 = {x}");
+
+            int z = Calculator.Plus(7, 8, 9);
+            Console.WriteLine($"INT : 7 + 8 + 9 = {z}");
+
+            float y = Calculator.Plus(3.14f, 5.6f); //float
+            Console.WriteLine($"FLOAT : 3.14 + 5.6 = {y}");
+
+            Console.WriteLine($"DOUBLE : 3.14 + 5.6 = {Calculator.Plus(3.14, 5.6)}");//double
+
+            Console.WriteLine($"CHAR : 3.14 + 5 = {Calculator.Plus(3.14, "5")}");
+
+            //오버로딩의 문제점 -> 가변길이 매개변수 params 키워드와 배열 이용
+            
+            int total = 0;
+            total = sum(1, 2);
+            Console.WriteLine($"합계는 {total}");
+
+            total = sum(1, 2, 3);
+            Console.WriteLine($"합계는 {total}");
+
+            total = sum(1, 2, 3, 4);
+            Console.WriteLine($"합계는 {total}");
+
+            total = sum(1, 2, 3, 4, 5);
+            Console.WriteLine($"합계는 {total}");
+
+            Console.WriteLine($"10까지의 합은 : {sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)}");
+
+            int[] arrs = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            Console.WriteLine($"10까지의 합은 : {sum(arrs)}");
+        }
+
+        private static int sum(params int[] args)
+        {
+            int result = 0;
+            foreach (var arg in args)
+            {
+                result += arg;
+            }
+            return result;
+        }
+
+        /*
+        private static int sum(int v1, int v2, int v3, int v4, int v5)
+        {
+            int result = v1 + v2 + v3 + v4 + v5;
+            return result;
+        }
+
+        private static int sum(int v1, int v2, int v3, int v4)
+        {
+            int result = v1 + v2 + v3 + v4;
+            return result;
+        }
+
+        private static int sum(int v1, int v2, int v3)
+        {
+            int result = v1 + v2 + v3;
+            return result;
+        }
+
+        private static int sum(int v1, int v2)
+        {
+            int result = v1 + v2;
+            return result;
+        }
+        */
+
+        private static object Plus(double v1, string v2)
+        {
+            double.TryParse(v2, out double p2);
+            double result = v1 + p2;
+            return result;
+        }
+
+        private static object Plus(double v1, double v2) //바로 출력하게 돼서 어떤 자료형으로 반환하는 지 몰라서 object로 생성됨(박싱)
+        {
+            double result = v1 + v2;
+            return result;
+        }
+
+        private static float Plus(float v1, float v2)
+        {
+            float result = v1 + v2;
+            return result;
+        }
+
+        private static int Plus(int v1, int v2)
+        {
+            int result = v1 + v2;
+            return result;
+        }
+
+        private static int Plus(int v1, int v2, int v3)
+        {
+            int result = v1 + v2 + v3;
+            return result;
+        }
+
+    }
+}
---
+++ chap06/Chapo06App/ProfileTestApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintProfile("성명건", "010-6683-7732");
+
+            PrintProfile(null, "010-1111-2222");
+
+            PrintProfile(phone: "010-9999-9999", name: "홍길동");
+
+            PrintProfile(name:"홍길순",phone:"010-8888-8888");
+
+            PrintProfile("최백호");
+            PrintProfile("최백호", "010-4444-5555");
+        }
+
+        public static void PrintProfile(string name, string phone = "010-0000-0000") //디폴트 파라미터, 뒤에서부터 넣어야함(매개변수 갯수 판단 문제)!
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("$$ 오류 : 이름을 정확히 입력하세요.");
+                return;
+            }
+            Console.WriteLine($"이름 : {name}, 번호 : {phone}");
+        }
+
+    }
+}
