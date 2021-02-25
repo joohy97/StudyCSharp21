3d6603a86acbcd82e1dfed7c2f668c89b8950454
joohy <joohyun104@gmail.com>
2021-02-25 12:30:44 +0900

    0225 12:30

---
+++ chap07/Chap07App/ClassTypeCastApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTypeCastApp
+{
+    class 포유류
+    {
+        public void 키우다()
+        {
+            Console.WriteLine("키우다()");
+        }
+    }
+
+    class 강아지 : 포유류
+    {
+        public void 멍멍()
+        {
+            Console.WriteLine("멍멍()");
+        }
+    }
+
+    class 고양이 : 포유류
+    {
+        public void 야옹()
+        {
+            Console.WriteLine("야옹()");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            포유류 _포유류 = new 포유류();
+            _포유류.키우다();
+
+            _포유류 = new 강아지(); //형변환 부모(big)<-자식(small). 자식을 부모에 담음. 포유류(부모)였던 _포유류가 강아지(자식)으로 형변환
+            _포유류.키우다();
+            강아지 쭈쭈;
+
+            if (_포유류 is 강아지) //true
+            {
+                쭈쭈 = _포유류 as 강아지;
+                쭈쭈.멍멍();
+            }
+
+            강아지 뽀삐 = null;
+            if (뽀삐 is 포유류) //false
+            {
+                Console.WriteLine("실행");
+                뽀삐 = new 포유류() as 강아지;
+                뽀삐.키우다();
+                뽀삐.멍멍();
+            }
+
+
+            /* 실행 오류
+            //강아지 뽀삐 = new 포유류(); //컴파일 오류 자식(small)<-부모(big)
+            강아지 뽀삐 = (강아지) new 포유류(); //형변환 자식(small)<-부모(big) 가능
+            뽀삐.키우다();
+            //뽀삐.멍멍(); 
+            //형변환으로 부모 클래스로 만들어도 자식 클래스의 메소드 호출 가능하지만 실행 오류
+            //본래 부모 클래스에 멍멍이라는 메소드가 없기 때문
+
+            고양이 로미 = (고양이) new 포유류();
+            //로미.야옹();
+            */
+
+
+        }
+    }
+}
---
+++ chap07/Chap07App/Override/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Override
+{
+    class ArmorSuite
+    {
+        public virtual void Initialize() //virtual : 자식클래스에서 재정의할 것 미리 알려줌
+        {
+            Console.WriteLine("ArmorSuite 초기화");
+        }
+    }
+
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            //base.Initialize();
+            Console.WriteLine("리펄서 레이저 장착");
+        }
+    }
+
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            //base.Initialize();
+            Console.WriteLine("ArmorSuite 개량 WarMachine 초기화");
+            Console.WriteLine("Double-Barrel 캐논 장착");
+            Console.WriteLine("Micro-Rocket 런처 장착");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ArmorSuite 생산");
+            ArmorSuite suite = new ArmorSuite();
+            suite.Initialize();
+
+            Console.WriteLine("WarMachine 생산");
+            WarMachine machine = new WarMachine();
+            machine.Initialize();
+
+            Console.WriteLine("IronMan 생산");
+            IronMan ironMan = new IronMan();
+            ironMan.Initialize();
+        }
+    }
+}
