3d5c535ded26578a4e949c6d9b868e575a8191c6
joohy <joohyun104@gmail.com>
2021-03-02 10:34:37 +0900

    0302

---
+++ chap10/ArrayListApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            ArrayList array = new ArrayList();
+            array.Add(80);
+            array.Add(74);
+            array.Add(81);
+            array.Add(90);
+            array.Add(34);
+            */
+
+            ArrayList array = new ArrayList(new int[] { 80, 74, 81, 90, 34 });
+
+            var LOC = array.IndexOf(81);
+        }
+    }
+}
---
+++ chap10/HashTableApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht["일"] = "Uno";
+            ht["이"] = "Dos";
+            ht["삼"] = "Tres";
+            ht["사"] = "Quatro";
+
+            Console.WriteLine(ht["일"]);
+            Console.WriteLine(ht["이"]);
+            Console.WriteLine(ht["삼"]);
+        }
+    }
+}
