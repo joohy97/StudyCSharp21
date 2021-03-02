5cbb17b6080f82423604337822cb0c61f61fc6fb
joohy <joohyun104@gmail.com>
2021-03-02 17:25:26 +0900

    오늘 수업 끗

---
+++ chap13/CallbackTestApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CallbackTestApp
+{
+    delegate int Compare(int a, int b);
+    class Program
+    {
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b) return 1;
+            else if (a == b) return 0;
+            else return -1;
+        }
+
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b) return 1;
+            else if (a == b) return 0;
+            else return -1;
+        }
+
+        static void BubbleSort(int[] DataSet, Compare comparer)
+        {
+            int temp = 0;
+            for (int i = 0; i < DataSet.Length; i++)
+            {
+                for (int j = 0; j < DataSet.Length - (i+1); j++)
+                {
+                    if (comparer(DataSet[j], DataSet[j+1]) > 0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                    
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 8, 4, 2, 1, 10 };
+
+            Console.WriteLine("Sorting...");
+            BubbleSort(array, new Compare(AscendCompare));
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ chap13/Chap13App/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap13App
+{
+    delegate int MyDelegate(int a, int b); //대리자, 대신 호출
+
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            /*
+            var result = calc.Plus(3, 4);
+            Console.WriteLine($"result : {result}");
+            */
+
+            MyDelegate Callback;
+            Callback = new MyDelegate(calc.Plus); //메서드인데 괄호 없음
+            Console.WriteLine($"result : {Callback(3,4)}");
+        }
+    }
+}
