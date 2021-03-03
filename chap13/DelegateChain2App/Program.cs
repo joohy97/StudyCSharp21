2919ea8838b8d7d8d1843ee0557f97093eff38b0
joohy <joohyun104@gmail.com>
2021-03-03 10:26:01 +0900

    0303 10:25

---
+++ chap13/DelegateChain2App/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateChain2App
+{
+    class Program
+    {
+        delegate void ThereIsAFire(string location);
+
+        static void Call119(string location)
+        {
+            Console.WriteLine($"소방서죠? 불났어요, 주소는 {location}이에요!");
+        }
+
+        static void ShoutOut(string location)
+        {
+            Console.WriteLine($"불이야!! {location}에 불났어요!");
+        }
+
+        static void Escape(string location)
+        {
+            Console.WriteLine($"{location}에서 나가야해요!");
+        }
+
+        static void Main(string[] args)
+        {
+            ThereIsAFire fire = new ThereIsAFire(ShoutOut);
+            fire += Call119;
+            fire += Escape;
+
+            fire("부경대학교 대연캠퍼스 C23");
+        }
+    }
+}
---
+++ chap13/UsingEventApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace UsingEventApp
+{
+    delegate void EventHandler(string message); //메세지 받아서 대신 처리하는 대리자
+
+    class CustomNotifier //이벤트 선언, 사용하는 객체(속성+메서드)
+    {
+        public event EventHandler SomethingHappened; //속성
+
+        public void DoSomething(int number) //메서드
+        {
+            int temp = number % 10;
+
+            if (temp != 0 && temp % 3 == 0) //3의 배수
+            {
+                SomethingHappened($"{number} : 짝!");
+            }
+            else
+            {
+                SomethingHappened($"{number}");
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            CustomNotifier notifier = new CustomNotifier();
+            notifier.SomethingHappened += new EventHandler(MyHandler); //이벤트를 내가 만든 로직이 있는 메서드랑 연결
+
+            for (int i = 0; i < 100; i++)
+            {
+                notifier.DoSomething(i);
+            }
+        }
+
+    }
+}
